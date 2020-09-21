899ef75760832bbda863ef9cb1847d09a415be1e
GotoRen <r0719en@pluslab.org>
2020-09-22 05:26:07 +0900

    add, lesson01

--- 01/lesson01.go
+++ 01/lesson01.go
@@ -1,12 +1,17 @@
+/* import */
 package main
 
 import (
 	"fmt"
+	"math"
 	"os/user"
 	"time"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println(time.Now())
 	fmt.Println(user.Current())
+	fmt.Println(rand.Intn(10)) // 常に同じ値を返す
+	fmt.Printf("%g\n", math.Sqrt(7))
 }
