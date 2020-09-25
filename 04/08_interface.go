d40267bdec0a014a075b3c8e7e88321024d53f84
GotoRen <r0719en@pluslab.org>
2020-09-26 07:33:49 +0900

    add, {Methods and interfaces.}

--- 04/08_interface.go
+++ 04/08_interface.go
@@ -2,38 +2,21 @@
 package main
 
 import (
-	"fmt"
-	"math"
-)
-
-package main
-
-import (
 "fmt"
 "math"
 )
 
-type Abser interface {
-	Abs() float64
-}
-
-func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f  // a MyFloat implements Abser
-	a = &v // a *Vertex implements Abser
-
-	// In the following line, v is a Vertex (not *Vertex)
-	// and does NOT implement Abser.
-	a = v
-
-	fmt.Println(a.Abs())
-}
-
+/* 構造体 */
+/*********************************/
 type MyFloat float64
 
+type Vertex struct {
+	X, Y float64
+}
+/*********************************/
+
+/* メソッド */
+/*********************************/
 func (f MyFloat) Abs() float64 {
 	if f < 0 {
 		return float64(-f)
@@ -41,10 +24,41 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
-type Vertex struct {
-	X, Y float64
-}
-
 func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
+/*********************************/
+
+/* インターフェース */
+/*********************************/
+type Abser interface {
+	Abs() float64
+}
+/*********************************/
+
+func main() {
+	var a Abser
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println("f: ", f)
+	a = f  // MyFloatはAbserを実装する
+	fmt.Println("a: ", a)
+	fmt.Println("a.Abs(): ", a.Abs())
+
+	fmt.Println()
+
+	v := Vertex{3, 4}
+	fmt.Println("v: ", v)
+	a = &v // VertexはAbserを実装する
+	fmt.Println("a(&v): ", a)
+	fmt.Println("a.Abs(): ", a.Abs())
+}
+/* 出力
+> f:  -1.4142135623730951
+> a(f):  -1.4142135623730951
+> a.Abs():  1.4142135623730951
+
+> v:  {3 4}
+> a(&v):  &{3 4}
+> a.Abs():  5
+*/
--- 04/10_implicit-interface.go
+++ 04/10_implicit-interface.go
@@ -1,22 +1,26 @@
+/* implicit interface */
 package main
 
 import "fmt"
 
-type INTERFACE interface {
-	METHOD()
-}
-
+/* 構造体 */
 type TYPE struct {
 	STRING string
 }
 
+/* メソッド */
 // 暗黙的にタイプTYPEがインターフェースINTERFACEを実装する（明示的に宣言する必要はない）
 func (t TYPE) METHOD() {
 	fmt.Println(t.STRING)
 }
 
+/* インターフェース */
+type INTERFACE interface {
+	METHOD()
+}
+
 func main() {
 	var i INTERFACE // 変数INTERFACEはインターフェース型
-	i = TYPE{"Hello, World!"}
-	i.METHOD()
+	i = TYPE{"Hello, World!"} // 代入
+	i.METHOD() // 出力
 }
